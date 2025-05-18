eac18f7a97c7ff941acf35e6f5a44d9859c6cd28
inu255 <artmvhdv@mail.ru>
2025-05-18 18:09:37 +0300

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
